9d75b5bfd8bdcbcc5cbfde03c0536bef27f1797a
nav-bioinformatics2128 <navkaur21228@gmail.com>
2026-04-26 18:18:02 +0530

    Update dna_analyzer.py
    Add reverse and complement features

--- dna_analyzer.py
+++ dna_analyzer.py
@@ -18,3 +18,34 @@ print("Base counts:", counts)
 for base in counts:
     percent = (counts[base] / total) * 100
     print(base + "%:", percent)
+# Reverse DNA
+reverse = dna[::-1]
+print("Reverse:", reverse)
+
+# Complement DNA
+complement = ""
+for base in dna:
+    if base == "A":
+        complement += "T"
+    elif base == "T":
+        complement += "A"
+    elif base == "G":
+        complement += "C"
+    elif base == "C":
+        complement += "G"
+
+print("Complement:", complement)
+
+# Reverse Complement
+rev_comp = ""
+for base in reverse:
+    if base == "A":
+        rev_comp += "T"
+    elif base == "T":
+        rev_comp += "A"
+    elif base == "G":
+        rev_comp += "C"
+    elif base == "C":
+        rev_comp += "G"
+
+print("Reverse Complement:", rev_comp)
