380d155a0f1bb4627ef3e1a96fa9a69b5b43d9c1
nav-bioinformatics2128 <navkaur21228@gmail.com>
2026-06-05 08:05:17 +0530

    Add mutation detection between two sequences

---
+++ mutation_details.py
@@ -0,0 +1,18 @@
+from Bio.Seq import Seq
+
+seq1 = Seq("ATGCGAGA")
+seq2 = Seq("ATGCCATA")
+
+mutations = []
+
+for i in range(len(seq1)):
+
+    if seq1[i] != seq2[i]:
+
+        mutation = "Position " + str(i) + ": " + str(seq1[i]) + "->" + str(seq2[i])
+
+        mutations.append(mutation)
+
+for mutation in mutations:
+
+    print(mutation)
