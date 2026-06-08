00c850e66da6f28e1bf0c5ba0c0cdcb74f8f1422
nav-bioinformatics2128 <navkaur21228@gmail.com>
2026-06-08 20:07:45 +0530

    Add sequence comparison and summary generation script
    This script reads a FASTA file, compares sequences in pairs, and calculates their differences and similarity percentages. It stores the results in a list of summaries.

---
+++ all_sequence_summaries.py
@@ -0,0 +1,31 @@
+from Bio import SeqIO
+
+records = list(SeqIO.parse("sample.fasta", "fasta"))
+
+all_summaries = []
+
+for i in range(0, len(records), 2):
+
+    seq1 = records[i].seq
+    seq2 = records[i + 1].seq
+
+    differences = 0
+
+    for j in range(len(seq1)):
+
+        if seq1[j] != seq2[j]:
+
+            differences += 1
+
+    similarity = ((len(seq1) - differences) / len(seq1)) * 100
+
+    summary = {
+        "Pair": records[i].id + " vs " + records[i + 1].id,
+        "Length": len(seq1),
+        "Differences": differences,
+        "Similarity": round(similarity, 2)
+    }
+
+    all_summaries.append(summary)
+
+print(all_summaries)
