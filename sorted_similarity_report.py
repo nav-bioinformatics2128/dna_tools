c7b23370c9dd4e788e279cb047a1faafc8201ae7
nav-bioinformatics2128 <navkaur21228@gmail.com>
2026-06-08 20:10:54 +0530

    Add script to generate sorted similarity report

---
+++ sorted_similarity_report.py
@@ -0,0 +1,48 @@
+from Bio import SeqIO
+
+records = list(SeqIO.parse("sample.fasta", "fasta"))
+
+all_summaries = []
+
+for i in range(0, len(records), 2):
+
+    seq1 = records[i].seq
+    seq2 = records[i + 1].seq
+
+    differences = 0
+
+    for j in range(len(seq1)):
+
+        if seq1[j] != seq2[j]:
+
+            differences += 1
+
+    similarity = ((len(seq1) - differences) / len(seq1)) * 100
+
+    summary = {
+        "Pair": records[i].id + " vs " + records[i + 1].id,
+        "Similarity": round(similarity, 2)
+    }
+
+    all_summaries.append(summary)
+
+all_summaries.sort(
+    key=lambda x: x["Similarity"],
+    reverse=True
+)
+
+report = open("sorted_report.txt", "w")
+
+for item in all_summaries:
+
+    report.write(item["Pair"] + "\n")
+
+    report.write(
+        "Similarity: " +
+        str(item["Similarity"]) +
+        "%\n\n"
+    )
+
+report.close()
+
+print("Report Created")
