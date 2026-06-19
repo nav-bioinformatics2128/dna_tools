ce59903ef2510fad246873f1843792da02f1f943
nav-bioinformatics2128 <navkaur21228@gmail.com>
2026-06-19 18:48:57 +0530

    Create sort_by_gc.py

---
+++ sort_by_gc.py
@@ -0,0 +1,29 @@
+from Bio import SeqIO
+
+results = []
+
+for record in SeqIO.parse("sample.fasta", "fasta"):
+
+    sequence = str(record.seq)
+
+    g_count = sequence.count("G")
+
+    c_count = sequence.count("C")
+
+    gc_percent = ((g_count + c_count)
+                  / len(sequence)) * 100
+
+    results.append({
+        "ID": record.id,
+        "GC": gc_percent
+    })
+
+results.sort(
+    key=lambda item: item["GC"],
+    reverse=True
+)
+
+for item in results:
+
+    print(item["ID"],
+          round(item["GC"], 2))
